a16b1d475c82e8141127ad197a437b192e269cfe
Alessio Izzo <alessio.izzo86@gmail.com>
2024-09-25 00:16:14 +0200

    go solution with channels seems to work

--- golang/main.go
+++ golang/main.go
@@ -7,36 +7,60 @@ import (
 )
 
 type TaskQueue struct {
-	queue chan int
+	channel chan int
+	queue   chan int
 }
 
 func (tq *TaskQueue) enqueue(n int) {
-	tq.queue <- n
-	go promise_factory(n)
-
-}
-
-func newTaskQueue() *TaskQueue {
-	return &TaskQueue{
-		queue: make(chan int, 10),
+	select {
+	case tq.channel <- n:
+		go promise_factory(n, tq)
+	default:
+		tq.queue <- n
 	}
 }
 
-func promise_factory(n int) {
+func (tq *TaskQueue) tryToRunTask() {
+	if len(tq.channel) == 3 {
+		return
+	}
+	n := tq.dequeue()
+	tq.channel <- n
+	go promise_factory(n, tq)
+}
+
+func (tq *TaskQueue) dequeue() int {
+	return <-tq.queue
+}
+
+func (tq *TaskQueue) run() {
+
+	for i := 1; i < 10; i++ {
+		tq.enqueue(i)
+	}
+
+	for len(tq.channel) != 0 || len(tq.queue) != 0 {
+		go tq.tryToRunTask()
+	}
+
+}
+func newTaskQueue() *TaskQueue {
+	return &TaskQueue{
+		channel: make(chan int, 3),
+		queue:   make(chan int, 100),
+	}
+}
+
+func promise_factory(n int, tq *TaskQueue) {
 	number := rand.Intn(n) + 1
 	fmt.Println("Start promise", n)
 	time.Sleep(time.Duration(number) * time.Second)
-	fmt.Println("End promise", n, ". Took", number, "seconds")
+	fmt.Println("End promise", n, "Took", number, "seconds")
+	<-tq.channel
 }
 
 func main() {
 	q := newTaskQueue()
 
-	for i := 1; i < 10; i++ {
-		q.enqueue(i)
-	}
-
-	for item := range q.queue {
-		fmt.Println(item)
-	}
+	q.run()
 }
